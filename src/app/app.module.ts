80fe46bb01480874ebab3e615d0f897879ec01fb
unknown <sirpepega777@gmail.com>
2024-11-11 16:29:04 -0500

    actualización de email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import {environment} from "../environments/environment";
-import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-
-import {getAuth, provideAuth} from "@angular/fire/auth"
-import {getStorage, provideStorage} from "@angular/fire/storage"
+import {environment} from '../environments/environment'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {getAuth,provideAuth} from '@angular/fire/auth'
+import {getStorage, provideStorage} from '@angular/fire/storage'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideFirebaseApp(()=> initializeApp(environment.firebase)),
-    provideFirestore(()=> getFirestore())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideAuth(()=>getAuth()),
+  provideStorage(()=>getStorage()),
+  provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,36 @@
 import { Injectable } from '@angular/core';
 import {
-  Auth, 
+  Auth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut
-} from "@angular/fire/auth";
+} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(private auth: Auth) { }
-  async register({email, password}){
+
+  async register({ email, password }: { email: string; password: string }) {
     try {
       const user = await createUserWithEmailAndPassword(this.auth, email, password);
       return user;
-    } catch (error) {
-      return console.log(error);
+    } catch (e) {
+      return null;
     }
   }
 
-  async login({email, password}){
+  async login({ email, password }: { email: string; password: string }) {
     try {
       const user = await signInWithEmailAndPassword(this.auth, email, password);
       return user;
-    } catch (error) {
-      return console.log(error);
+    } catch (e) {
+      return null;
     }
   }
 
-  logOut(){
+  logout() {
     return signOut(this.auth);
   }
-
-
-
 }

f4a4ebde599dbbc6cf9a1bad1f6d994cbe4367dc
Alexis <paulpulamarin141220@gmail.com>
2024-11-11 17:02:34 -0500

    Home.page.ts update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { AvatarService } from '../services/avatar.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  profile = null;
+  constructor(
+    private avatarService: AvatarService,
+    private authService: AuthService,
+    private router: Router,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+  ) {
+    this.avatarService.getUserProfile().subscribe((data) => {
+      this.profile = data;
+    });
+  }
+  async logout() {
+    await this.authService.logout();
+    this.router.navigateByUrl('/', {
+      replaceUrl: true
+    });
+  }
+  async changeImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Photos
+    });
 
-  constructor() {}
+    if (image) {
+      const loading = await this.loadingController.create();
+      await loading.present();
 
+      const result = await this.avatarService.uploadImage(image);
+      loading.dismiss();
+
+      if (!result) {
+        const alert = await this.alertController.create({
+          header: 'Upload failed',
+          message: 'Existe un problema al cargar tu imagen.',
+          buttons: ['OK']
+        });
+        await alert.present();
+      }
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,15 +13,24 @@ export const environment = {
     // appId: "1:153829575850:web:ce11c0a4f3cc0c9b5694d3",
     // measurementId: "G-V7MXNE52JV"
     
-  
-    apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
-    authDomain: "b-1accc.firebaseapp.com",
-    projectId: "b-1accc",
-    storageBucket: "b-1accc.firebasestorage.app",
-    messagingSenderId: "606986248368",
-    appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
-    measurementId: "G-QDN8GEEH17"
+    //inge
+    // apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
+    // authDomain: "b-1accc.firebaseapp.com",
+    // projectId: "b-1accc",
+    // storageBucket: "b-1accc.firebasestorage.app",
+    // messagingSenderId: "606986248368",
+    // appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
+    // measurementId: "G-QDN8GEEH17"
 
+    
+      apiKey: "AIzaSyAJn_tGbz44coLI1vINPmbJMCOkaEBts5s",
+      authDomain: "balexis-8a07a.firebaseapp.com",
+      projectId: "balexis-8a07a",
+      storageBucket: "balexis-8a07a.firebasestorage.app",
+      messagingSenderId: "103493038467",
+      appId: "1:103493038467:web:46ec24b9a4c454083dc6b6",
+      measurementId: "G-GCW29L1C8H"
+    
 
   }
 };

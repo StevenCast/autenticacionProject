9064459f1393355731fb84a0d1ed23927a56794c
Alexis Farinango <paulpulamarin141220@gmail.com>
2024-11-14 21:37:59 -0500

    Actualizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,32 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    // apiKey: "AIzaSyA_8ug7yZggG3mP9B-wbYtk8oa9m9Ur6-o",
-    // authDomain: "b-steven.firebaseapp.com",
-    // projectId: "b-steven",
-    // storageBucket: "b-steven.firebasestorage.app",
-    // messagingSenderId: "153829575850",
-    // appId: "1:153829575850:web:ce11c0a4f3cc0c9b5694d3",
-    // measurementId: "G-V7MXNE52JV"
-    
-    //inge
-    apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
-    authDomain: "b-1accc.firebaseapp.com",
-    projectId: "b-1accc",
-    storageBucket: "b-1accc.firebasestorage.app",
-    messagingSenderId: "606986248368",
-    appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
-    measurementId: "G-QDN8GEEH17"
-
-    
-      // apiKey: "AIzaSyAJn_tGbz44coLI1vINPmbJMCOkaEBts5s",
-      // authDomain: "balexis-8a07a.firebaseapp.com",
-      // projectId: "balexis-8a07a",
-      // storageBucket: "balexis-8a07a.firebasestorage.app",
-      // messagingSenderId: "103493038467",
-      // appId: "1:103493038467:web:46ec24b9a4c454083dc6b6",
-      // measurementId: "G-GCW29L1C8H"
-    
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 
   }
 };
